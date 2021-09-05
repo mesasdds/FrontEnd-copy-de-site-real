7ea1259091227d278176fcee43022b8fb3c3ec91
mesasdds <48107212+mesasdds@users.noreply.github.com>
2021-09-05 17:20:08 -0300

    final

--- README.md
+++
@@ -1,2 +0,0 @@
-# estagios
-estagio rocky
---
+++ sidebar.js
@@ -0,0 +1,9 @@
+
+
+
+const menuSection = document.querySelector(".menu-section2")
+const menuBurguer = menuSection.querySelector(".menu-hamburguer")
+
+menuBurguer.addEventListener("click", function click(){
+    menuSection.classList.toggle("on")
+})
